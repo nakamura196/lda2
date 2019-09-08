afa5c207d6e26673ee70acb1c6bd646709b9fd48
Satoru Nakamura <na.kamura.1263@gmail.com>
2019-09-08 09:57:44 +0900

    add manifest

--- src/batch.sh
+++
@@ -1,8 +0,0 @@
-python 01_create_metadata.py data/oml/data/config.yml
-python 02_create_images.py data/oml/data/config.yml
-python 12_create_manifest.py data/oml/data/config.yml
-python 13_create_collection.py data/oml/data/config.yml
-python 20_collection_converter.py /Users/nakamura/git/min_a/lda2/docs/oml/collection.json /Users/nakamura/git/min_a/lda2/docs/oml/items.json
-python 21_collection_converter4es.py /Users/nakamura/git/min_a/lda2/docs/oml/collection.json /Users/nakamura/git/min_a/lda2/docs/oml/rows.json
-python 30_create_uni.py data/oml/data/config.yml
-python 41_create_rows.py
