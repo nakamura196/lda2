616dc0870d1283b4f994441604d63213b4618dc2
Satoru Nakamura <na.kamura.1263@gmail.com>
2019-09-08 13:54:02 +0900

    add oml

--- collections_data/oml/create_images.py
+++ collections_data/oml/create_images.py
@@ -30,31 +30,26 @@ df = pd.read_excel(path, sheet_name=0, header=None, index_col=None)
 r_count = len(df.index)
 c_count = len(df.columns)
 
-map = {}
-
-rows = []
-row = ["id", "img_url", "thumb_url", "width", "height"]
-rows.append(row)
-
-rows2 = []
-row2 = ["id", "thumb_url"]
-rows2.append(row2)
-
 for j in range(1, r_count):
 
-    if j < 4371:
-        continue
-
-    id = df.iloc[j, 0]
     num = df.iloc[j, 87]
 
-    print(str(j)+"/"+str(r_count))
-
     if not pd.isnull(num) and num != 0:
-        url = "http://image.oml.city.osaka.lg.jp/archive/detail.do?id="+str(id)
-        print(url)
 
-        time.sleep(1)
+        id = df.iloc[j, 0]
+
+        filename = "data/images/"+str(id)+".json"
+
+        if os.path.exists(filename):
+            continue
+
+        print(str(j)+"/"+str(r_count))
+
+        main = {}
+        array = []
+        main["array"] = array
+
+        url = "http://image.oml.city.osaka.lg.jp/archive/detail.do?id="+str(id)
 
         r = requests.get(url)  # requestsを使って、webから取得
 
@@ -62,18 +57,11 @@ for j in range(1, r_count):
 
         imgs = soup.find_all("img")
 
-        print("画像数: "+str(len(imgs)-11))
-
-        thumb_flg = True
-
-        thumb_url = "http://image.oml.city.osaka.lg.jp/archive/get-thumbnail?data_no="+str(id)
-                    
-        row2 = [id, thumb_url]
-        rows2.append(row2)
+        thumb_url = "http://image.oml.city.osaka.lg.jp/archive/get-thumbnail?data_no=" + \
+            str(id)
 
         for img in imgs:
             src = img.get("src")
-            
 
             if "data_no" in src and "get-large" in src:
                 # print(src)
@@ -81,42 +69,17 @@ for j in range(1, r_count):
                 img_url = "http://image.oml.city.osaka.lg.jp/archive/get-media?data_no="+data_no
                 thumb_url = "http://image.oml.city.osaka.lg.jp/archive/get-middle?data_no="+data_no
 
-                print(img_url)
-
                 image = Image.open(urllib.request.urlopen(img_url))
                 width, height = image.size
 
-                row = [id, img_url, thumb_url, width, height]
-                rows.append(row)
+                obj = {
+                    "img_url": img_url,
+                    "thumb_url": thumb_url,
+                    "width": width,
+                    "height": height
+                }
+                array.append(obj)
 
-                    
-
-                    
-
-    # break
-
-    if j % 10 == 0:
-
-        f = open("data/images/images_"+str(j).zfill(5)+".csv", 'w')
-
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerows(rows)
-
-        f.close()
-
-        f = open("data/thumbnails/thumbnails_"+str(j).zfill(5)+".csv", 'w')
-
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerows(rows2)
-
-        f.close()
-
-        rows = []
-        row = ["id", "img_url"]
-        rows.append(row)
-
-        rows2 = []
-        row2 = ["id", "thumb_url"]
-        rows2.append(row2)
-
-
+        f2 = open(filename, 'w')
+        json.dump(main, f2, ensure_ascii=False, indent=4,
+                  sort_keys=True, separators=(',', ': '))
